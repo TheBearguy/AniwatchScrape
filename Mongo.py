95fa8ae02efcb691d567f54497573bd9fc8cac32
Shreyansh Shukla <bugbreaker18@gmail.com>
2023-09-04 11:13:04 +0530

    Update Mongo.py
    Deleted unused code

--- Mongo.py
+++ Mongo.py
@@ -1,40 +1,25 @@
 import requests
 from pymongo import MongoClient
 from dotenv import load_dotenv
-import os 
+import os
 
 load_dotenv()
-
-api_url = os.getenv("API_URL")
 mongodb_uri = os.getenv("MONGODB_URI")
+api_url = os.getenv("API_URL")
 
 client = MongoClient(mongodb_uri)
 
-db = client['mydatabase']
-collection = db['mycollection'] 
+db = client['aniwatchScrape']
+collection = db['aniwatchScrape']
 
-data_structure = {
-    'title': str,
-    'img': str,
-    'type': str, 
-    'time': str, 
-    'quality': str, 
-    'date': str, 
-    'description': str,
-}
 
-def fetch_data():
+def get_anime():
     try:
-        response = requests.get(api_url)
-        api_data = response.json()
-
-        collection = db['mycollection'] 
-        collection.insert_many(api_data)
-
-        print('Data added to MongoDB')
-    except Exception as error:
-        print('Error fetching data:', error)
+        anime = requests.get(api_url).json()
+        collection = db['aniwatchScrape']
+        collection.insert_many(anime)
+    except Exception as e:
+        print(e)
     finally:
         client.close()
-
-fetch_data()
+get_anime()
